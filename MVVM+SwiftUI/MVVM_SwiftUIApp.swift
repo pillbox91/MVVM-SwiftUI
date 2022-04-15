77579379250c4b550d6e9fb193af0c4cdcf12ea4
Andrey Averyanov <averyanov1996@gmail.com>
2022-04-15 15:40:07 +0300

    Initial Commit

---
+++ MVVM+SwiftUI/MVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_SwiftUIApp.swift
+//  MVVM+SwiftUI
+//
+//  Created by Андрей Аверьянов on 15.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
